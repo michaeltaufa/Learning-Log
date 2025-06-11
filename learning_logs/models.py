33884ae0ea11893ff1b0cfc047b781e893758b6d
michaeltaufa <michael.taufa@gmail.com>
2025-06-11 13:26:09 -0500

    Updated files.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
 class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
+
+
 
 class Entry(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
@@ -17,4 +20,11 @@ class Entry(models.Model):
         verbose_name_plural = 'entries'
 
     def __str__(self):
-        return f"{self.text[:50]}..."
+        result = ''
+
+        if len(self.text) <= 50:
+            result = f"{self.text}"
+        else:
+            result = f"{self.text[:50]}..."
+
+        return result 
---
+++ learning_logs/urls.py
@@ -0,0 +1,17 @@
+"""Defines URL patterns for learning_logs."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+        # Home Page:
+        path('', views.index, name='index'),
+
+        # Page that shows all topics:
+        path('topics/', views.topics, name='topics'),
+
+        # Detail for single Topic page:
+        path('topics/<int:topic_id>', views.topic, name='topic'),
+]

f65e4ccb12f92c311ef55a115c058e7b166e810f
michaeltaufa <michael.taufa@gmail.com>
2025-06-23 14:25:13 -0500

    User functionality added.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,13 @@
 """Defines URL patterns for accounts."""
 
 from django.urls import path, include
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
+        # Include dafault auth urls
         path('', include('django.contrib.auth.urls')),
+
+        # User Registration Page
+        path('register/', views.register, name='register'),
 ]
